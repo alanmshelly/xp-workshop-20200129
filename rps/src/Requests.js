6fae01dec4012a2c19b34e329005234708796aaa
Derek Lee <derek@sunrisingappdev.com>
2020-01-29 17:18:35 +0900

    RPSRound method object refactoring and Choice constants

--- rps/src/Requests.js
+++ rps/src/Requests.js
@@ -1,24 +1,51 @@
 export class Requests {
     playRPS(p1Choice, p2Choice, observer) {
+        const rpsRound = new RPSRound(p1Choice, p2Choice, observer)
+        rpsRound.process()
+    }
+}
+
+class RPSRound {
+    constructor(p1Choice, p2Choice, observer) {
+        this.p1Choice = p1Choice
+        this.p2Choice = p2Choice
+        this.observer = observer
+    }
+
+    process() {
         if (
-            this.isInvalidChoice(p1Choice) ||
-            this.isInvalidChoice(p2Choice)
+            this.isInvalidChoice(this.p1Choice) ||
+            this.isInvalidChoice(this.p2Choice)
         ) {
-            observer.invalid()
-        } else if (p1Choice === p2Choice) {
-            observer.tie()
+            this.observer.invalid()
+        } else if (this.tieScenario()) {
+            this.observer.tie()
         } else if (
-            p1Choice === 'rock' && p2Choice === 'scissors' ||
-            p1Choice === 'scissors' && p2Choice === 'paper' ||
-            p1Choice === 'paper' && p2Choice === 'rock'
+            this.playerOneWinsScenarios()
         ) {
-            observer.p1Win()
+            this.observer.p1Win()
         } else {
-            observer.p2Win()
+            this.observer.p2Win()
         }
     }
 
-    isInvalidChoice(playerChoice) {
-        return !['rock', 'scissors', 'paper'].includes(playerChoice)
+    tieScenario() {
+        return this.p1Choice === this.p2Choice
     }
+
+    playerOneWinsScenarios() {
+        return this.p1Choice === Choice.ROCK && this.p2Choice === Choice.SCISSORS ||
+          this.p1Choice === Choice.SCISSORS && this.p2Choice === Choice.PAPER ||
+          this.p1Choice === Choice.PAPER && this.p2Choice === Choice.ROCK
+    }
+
+    isInvalidChoice(playerChoice) {
+        return ![Choice.ROCK, Choice.SCISSORS, Choice.PAPER].includes(playerChoice)
+    }
+}
+
+const Choice = {
+    ROCK: 'rock',
+    SCISSORS: 'scissors',
+    PAPER: 'paper',
 }
