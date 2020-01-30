9aa16c75c813f280d69c38d046dbf013ca866b6a
Alan Shelly <alan.m.shelly@gmail.com>
2020-01-30 10:31:17 +0900

    Initial test and refactor

--- web/spec/RPSAppSpec.jsx
+++ web/spec/RPSAppSpec.jsx
@@ -1,5 +1,59 @@
-describe("wiring test", () => {
-    it("works", () => {
-        expect(true).toBe(true)
+import React from 'react'
+import ReactDOM from 'react-dom'
+import RPSApp from '../src/RPSApp'
+
+describe('play form', () => {
+    let domFixture
+
+    beforeEach(() => {
+        setupDom()
     })
+
+    afterEach(() => {
+        teardownDom()
+    })
+
+    describe('when the play use case tells the UI that the input is invalid', () => {
+        beforeEach(() => {
+            const alwaysInvalidRequests = {
+                play: (p1Choice, p2Choice, observer) => observer.invalid(),
+            }
+            renderApp(alwaysInvalidRequests)
+        })
+
+        it('by default does not display the game result', () => {
+            expect(page()).not.toContain('INVALID!')
+        })
+
+        it('tells the user that their input is invalid', () => {
+            submitForm()
+
+
+            expect(page()).toContain('INVALID!')
+        })
+    })
+
+    function setupDom() {
+        domFixture = document.createElement('div')
+        document.body.appendChild(domFixture)
+    }
+
+    function teardownDom() {
+        domFixture.remove()
+    }
+
+    function renderApp(requests) {
+        ReactDOM.render(
+            <RPSApp requests={requests}/>,
+            domFixture
+        )
+    }
+
+    function page() {
+        return domFixture.textContent
+    }
+
+    function submitForm() {
+        domFixture.querySelector('button').click()
+    }
 })
--- web/src/RPSApp.jsx
+++ web/src/RPSApp.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
 export default class RPSApp extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            result: ''
+        }
+    }
+
+    handlePlayButtonClick() {
+        this.setState({
+            result: 'INVALID!'
+        })
+    }
+
     render() {
-        return <div>Nothing to see here...</div>
+        return <button onClick={this.handlePlayButtonClick.bind(this)}>
+            {this.state.result}
+        </button>
     }
 }
